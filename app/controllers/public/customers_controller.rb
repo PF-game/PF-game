2895299ca13c359087d0046753a8e046c085a3fa
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-06-13 13:07:59 +0000

    一覧の表示

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,24 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
     @customer = Customer.find(params[:id])
     @reviews = @customer.reviews
   end
-  
+
   def edit
+    @customer = Customer.find(params[:id])
   end
-  
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path(@customer.id)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :introduction, :profile_image)
+  end
+
 end
