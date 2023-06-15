96a418eda31b43b4bbf27c36de77dcb0c63c0bdb
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-06-15 08:04:56 +0000

    フラッシュメッセージ

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,4 +1,5 @@
 class Public::ReviewsController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def new
     @review = Review.new
@@ -8,6 +9,7 @@ class Public::ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.customer_id = current_customer.id
     if @review.save
+      flash[:notice] = "投稿が成功しました"
       redirect_to reviews_path
     else
       @reviews = Review.all
@@ -37,6 +39,7 @@ class Public::ReviewsController < ApplicationController
   def update
     @review = Review.find(params[:id])
     if @review.update(review_params)
+      flash[:notice] = "投稿が成功しました"
       redirect_to review_path(@review)
     else
       render :edit
@@ -48,6 +51,13 @@ class Public::ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:title, :body, :star)
+  end
+  
+  def is_matching_login_user
+    review = Review.find(params[:id])
+    unless review.id == current_customer.id
+      redirect_to reviews_path
+    end
   end
 
 end
