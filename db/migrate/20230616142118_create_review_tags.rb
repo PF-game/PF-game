de8ec444b4a30461e6a8bede8dcf41fa20ff1f37
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-06-18 10:39:22 +0000

    tag

--- db/migrate/20230616142118_create_review_tags.rb
+++ db/migrate/20230616142118_create_review_tags.rb
@@ -7,7 +7,7 @@ class CreateReviewTags < ActiveRecord::Migration[6.1]
       t.timestamps
     end
     # 同じタグは２回保存出来ない
-    add_index :review_tags, [:review_tag_id,:game_tag_id],unique: true
+    add_index :review_tags, [:review_id,:game_tag_id],unique: true
 
   end
 end
