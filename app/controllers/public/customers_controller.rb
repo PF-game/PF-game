4176b12cc6962229da339bc7c96553cae787008b
karasunogin <127410744+karasunogin@users.noreply.github.com>
2023-06-25 10:39:01 +0000

    マイページに遷移できないエラー修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,8 +5,6 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
     @reviews = @customer.reviews.page(params[:page])
-    @review = Review.find(params[:id])
-
   end
 
   def edit
